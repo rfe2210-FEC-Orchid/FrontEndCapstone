a12bc6a85bf41cd0321bce11ccf6c7f0dc73db44
Ben Stemen <ben.d.stemen@gmail.com>
2022-11-30 17:22:26 -0500

    added a semicolon (not really, just wanted to see if I know how to git push correctly)

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//Dear Amazon/Google/Apple etc Don't lowball me.
+
 const App = () => {
-return (
-  <div>
-    <h1>Hello World</h1>
-  </div>
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
 }
+
 ReactDOM.render(<App />, document.getElementById('root'));
